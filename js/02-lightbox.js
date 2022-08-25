314c68474a09048c8127442837733649b96a641f
IrynaKniazieva <gleb0812@gmail.com>
2022-08-25 16:59:57 +0300

    Task -2 work

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,34 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+// 2.1. находим куда вставить разметку
+const listGallery = document.querySelector('.gallery');
+// 2.2. ~ создаем карточки
+const cardsGallery = createGalleryCard (galleryItems);
+// 2.3. добавляем их в ul (gallery)
+listGallery.insertAdjacentHTML('beforeend', cardsGallery); 
+
+
+// 1. создаем разметку
+function createGalleryCard (galleryItems) {
+    return galleryItems.map(( { preview, original, description } ) => {
+return `
+<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+</li>
+`
+    }).join('');
+}
+
+let gallery = new SimpleLightbox('.gallery a', {captionsData: "alt", captionDelay: 250,});
+gallery.on('show.simplelightbox', function () {
+	// do something…
+});
+
